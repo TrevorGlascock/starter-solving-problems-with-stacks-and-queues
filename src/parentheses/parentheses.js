2ddb754da2459234e16b935fdd5f106311dfc03b
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-21 14:33:22 -0500

    First solution for Parenthesis

--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,20 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  const parens = new Stack();
+
+  for (let char of expression) {
+    // if the char is "(", push it into the stack
+    if (char === "(") parens.push(char);
+    if (char === ")") {
+      // if the char is a ")" and there is no matching "(", return false
+      if (!parens.top) return false;
+      parens.pop(); // otherwise, pop out the matching paren.
+    }
+  }
+
+  // return true if parens is empty, false otherwise
+  return !parens.top;
+};
 
 module.exports = match;
