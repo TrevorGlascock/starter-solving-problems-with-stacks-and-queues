8474743d932a47e38ebe849170a56c5e89e07f0e
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-21 13:55:12 -0500

    solved stack implementation of Palindromes

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -2,7 +2,18 @@ const Stack = require("../lib/stack");
 
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+
+  const midIndex = Math.floor(sentence.length / 2);
+  const charStack = new Stack();
+  // Populate stack with first half of sentence
+  for (let i = 0; i < midIndex; i++) charStack.push(sentence[i]);
+
+  // Even length sentences reflect in half, odd lengths reflect off the middle element
+  const reflIndex = sentence.length % 2 === 0 ? midIndex : midIndex + 1;
+  for (let i = reflIndex; i < sentence.length; i++)
+    if (charStack.pop() !== sentence[i]) return false;
+
+  return true;
 };
 
 module.exports = isPalindrome;
